eeb266c7cdb3813fe13f086cd2c346d42b9253df
John Gabriel <johngaelreyes@gmail.com>
2025-03-10 03:28:31 +0800

    Display characters - blocker operators

--- my-calculator/src/App.tsx
+++ my-calculator/src/App.tsx
@@ -12,6 +12,8 @@ function App() {
   const [waitingForSecondOperand, setWaitingForSecondOperand] =
     useState<boolean>(false);
 
+  const operations = ["+", "-", "x", "/", "%"];
+
   const onCLickKey = (e: React.MouseEvent<HTMLButtonElement>) => {
     const value = e.currentTarget.value;
 
@@ -22,17 +24,52 @@ function App() {
     if (value === "AC") {
       handleAllClear(value);
     }
+
+    if (value === "±") {
+      handleNegateOperation();
+    }
+
+    if (operations.includes(value)) {
+      handleOperation(value);
+    }
   };
 
   const handleNumberInput = (digit: string) => {
     setDisplayValue(
-      displayValue === "0" && digit !== "." ? digit : displayValue + digit
+      displayValue === "0" && digit !== "." ? digit : displayValue.concat(digit)
     );
+    setWaitingForSecondOperand(false);
   };
 
   const handleAllClear = (key: string) => {
     if (key === "AC") {
       setDisplayValue("0");
+    }
+  };
+
+  const handleOperation = (key: string) => {
+    if (displayValue === "0") return;
+
+    if (operations.includes(displayValue.slice(-1))) {
+      setDisplayValue(displayValue.slice(0, -1) + key);
+    } else {
+      setDisplayValue(displayValue + key);
+    }
+    // const numericValue = parseFloat(displayValue);
+    // setFirstOperand(numericValue);
+    // setWaitingForSecondOperand(true);
+  };
+
+  const handleNegateOperation = () => {
+    if (operations.includes(displayValue)) return;
+    const numericValue = parseFloat(displayValue);
+    const negateValue = numericValue * -1;
+    setDisplayValue(negateValue.toString());
+  };
+
+  const handleSecondOperand = (key: string) => {
+    if (operations.includes(displayValue)) {
+      console.log("secondOperand");
     }
   };
 
